d4bdbf93a8ad4f5393ed3fc9d0ef1a8b472f3319
Artur <artur.t@academico.ifrn.edu.br>
2022-10-14 17:37:58 -0300

    .

--- PEOO/PEOO/MainWindow.xaml.cs
+++ PEOO/PEOO/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PEOO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Olá");
+        }
     }
 }
