01732bf6e5984fa40ce7b7d85e7db3c3a93e756f
Artur <artur.t@academico.ifrn.edu.br>
2022-10-17 15:55:52 -0300

    Agenda

---
+++ PEOO/Aula_02/AgendaApp/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class Contato
+    {
+        private string nome, email, telefone;
+        public Contato(string n, string e, string t)
+        {
+            nome = n;
+            email = e;
+            telefone = t;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {telefone}";
+        }
+    }
+}
