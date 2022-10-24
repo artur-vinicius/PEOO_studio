43e0c6cd63af4d1b80812fd169786cf00059af2c
Artur <artur.t@academico.ifrn.edu.br>
2022-10-24 15:40:02 -0300

    Abastecimento

---
+++ PEOO/AbastecimentoApp/AbastecimentoApp/Abastecimento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbastecimentoApp
+{
+    class Abastecimento
+    {
+        private double valorLitro;
+        private double valorPago;
+        private double kmRodados;
+
+        public void SetValorLitro(double valorLitro)
+        {
+            if (valorLitro > 0) this.valorLitro = valorLitro;
+        }
+
+        public void SetValorPago(double valorPago)
+        {
+            if (valorPago > 0) this.valorPago = valorPago;
+        }
+        public void SetKmRodados(double kmRodados)
+        {
+            if (kmRodados > 0) this.kmRodados = kmRodados;
+        }
+        public double MediaKmLitro()
+        {
+            return kmRodados / (valorPago / valorLitro);
+        }
+        public double MediaReaisKm()
+        {
+            return valorPago / kmRodados;
+        }
+    }
+}
---
+++ PEOO/AbastecimentoApp/AbastecimentoApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AbastecimentoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento a = new Abastecimento();
+            a.SetKmRodados(double.Parse(txt_km.Text));
+            a.SetValorLitro(double.Parse(txt_vl.Text));
+            a.SetValorPago(double.Parse(txt_vp.Text));
+            txt_kml.Text = a.MediaKmLitro().ToString("0.00");
+            txt_RKm.Text = a.MediaReaisKm().ToString("0.00");
+        }
+    }
+}
